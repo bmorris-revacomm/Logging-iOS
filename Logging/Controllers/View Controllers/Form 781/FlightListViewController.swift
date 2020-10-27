b3eedcaace91bc9e966920dc99d05ca6246f7c84
Bethany Morris <bmorris@revacomm.com>
2020-10-27 17:30:41 -0500

    created table view for flight data, created subfolders

---
+++ Logging/Controllers/View Controllers/Form 781/FlightListViewController.swift
@@ -0,0 +1,105 @@
+//
+//  FlightListViewController.swift
+//  Logging
+//
+//  Created by Bethany Morris on 10/27/20.
+//  Copyright © 2020 Christian Brechbuhl. All rights reserved.
+//
+
+import UIKit
+
+class FlightListViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var flightDataTableView: UITableView!
+    @IBOutlet weak var popUpView: UIView!
+    
+    @IBOutlet weak var flightSeq: UITextField!
+    @IBOutlet weak var missionNumber: UITextField!
+    @IBOutlet weak var missionSymbol: UITextField!
+    @IBOutlet weak var fromICAO: UITextField!
+    @IBOutlet weak var toICAO: UITextField!
+    
+    @IBOutlet weak var takeOffTime: UITextField!
+    @IBOutlet weak var landTime: UITextField!
+    @IBOutlet weak var totalTime: UITextField!
+    
+    @IBOutlet weak var touchAndGo: UITextField!
+    @IBOutlet weak var fullStop: UITextField!
+    @IBOutlet weak var totalLandings: UITextField!
+    @IBOutlet weak var sorties: UITextField!
+    @IBOutlet weak var specialUse: UITextField!
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func newFlightButtonTapped(_ sender: UIButton) {
+        popUpView.isHidden = false
+    }
+    
+    @IBAction func exitButtonTapped(_ sender: UIButton) {
+        popUpView.isHidden = true
+    }
+    
+    @IBAction func addFlightButtonTapped(_ sender: UIButton) {
+        guard let flightSeq = flightSeq.text,
+              let missionNumber = missionNumber.text,
+              let missionSymbol = missionSymbol.text,
+              let fromICAO = fromICAO.text,
+              let toICAO = toICAO.text,
+              let takeOffTime = takeOffTime.text,
+              let landTime = landTime.text,
+              let totalTime = totalTime.text,
+              let touchAndGo = touchAndGo.text,
+              let fullStop = fullStop.text,
+              let totalLandings = totalLandings.text,
+              let sorties = sorties.text,
+              let specialUse = specialUse.text
+        else { return }
+        
+        FlightDataController.create(form: Form781Controller.shared.forms.last?, flightSeq: flightSeq, missionNumber: missionNumber, missionSymbol: missionSymbol, fromICAO: fromICAO, toICAO: toICAO, takeOffTime: takeOffTime, landTime: landTime, totalTime: totalTime, touchAndGo: touchAndGo, fullStop: fullStop, totalLandings: totalLandings, sorties: sorties, specialUse: specialUse)
+        popUpView.isHidden = true
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+} //End
+
+// MARK: - TableView Delegate
+
+extension FlightListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        Form781Controller.shared.forms.last?.flights.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.flightDataTableView.dequeueReusableCell(withIdentifier: "FlightCell", for: indexPath) as? FlightDataTableViewCell else { return UITableViewCell() }
+        
+        if let flight =  {
+            cell.setUpViews(flight: flight)
+        }
+        
+        return cell
+    }
+    
+} //End
